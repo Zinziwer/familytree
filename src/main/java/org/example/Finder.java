a757b76edaf7009e5236254ef21353d40e8c1125
Zinziwer <unislon@mail.ru>
2023-03-03 19:52:31 +0400

    dz1

---
+++ src/main/java/org/example/Finder.java
@@ -0,0 +1,9 @@
+package org.example;
+import java.util.List;
+
+public interface Finder {
+    public List getParents();
+    public List getChildren();
+    public List getSubParents();
+    public List getSubChildren();
+}
